cb17434bd6e0133482fcfe154a18f5e96f6a5864
yeonu365 <yeonustudios@gmail.com>
2021-06-04 16:53:14 +0900

    June

---
+++ WebContent/WEB-INF/sample2/board/list.jsp
@@ -0,0 +1,42 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"  %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="s2" tagdir="/WEB-INF/tags/sample2" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<%@ include file="/WEB-INF/subModules/bootstrapHeader.jsp" %>
+
+<title>Insert title here</title>
+</head>
+<body>
+<s2:navbar></s2:navbar>
+<div class="container">
+	<table class="table">
+	<h1>글 목록</h1>
+		<thead>
+			<tr>
+				<td>#</td>
+				<td>제목</td>
+				<td>작성자</td>
+				<td>작성시간</td>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach items="${boards }" var="board">
+				<tr>
+					<td>${board.id }</td>
+					<td>
+						<a href="<%= request.getContextPath() %>/sample2/board/detail?id=${board.id }">
+						${board.title }</a></td>
+					<td>${board.memberIdHidden }</td>
+					<td>${board.timeAgo }</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+</div>
+</body>
+</html>
---
+++ src/sample2/dao/BoardDao.java
@@ -0,0 +1,118 @@
+package sample2.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import sample2.bean.Board;
+
+public class BoardDao {
+	private String url;
+	private String user;
+	private String password;
+	
+	public BoardDao() {
+		this.url = "jdbc:mysql://52.79.189.42/test";
+		this.user = "root";
+		this.password = "wnddkdwjdqhcjfl1";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public boolean insert(Board board) {
+		String sql = "INSERT INTO Board "
+				+ "(title, body, memberId) "
+				+ "VALUES (?, ?, ?) ";
+		
+		try(
+			Connection con = DriverManager.getConnection(url, user, password);
+			PreparedStatement pstmt = con.prepareStatement(sql);
+				) {
+			pstmt.setString(1, board.getTitle());
+			pstmt.setString(2, board.getBody());
+			pstmt.setString(3, board.getMemberId());
+			
+			int cnt = pstmt.executeUpdate();
+			if (cnt ==1) {
+				return true;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public List<Board> list() {
+		List<Board> list = new ArrayList<>();
+		
+		String sql = "SELECT id, title, memberId, inserted FROM Board "
+				+ "ORDER BY id DESC ";
+		
+		try(
+			Connection con = DriverManager.getConnection(url, user, password);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+				) {
+			while (rs.next()) {
+				Board board = new Board();
+				board.setId(rs.getInt(1));
+				board.setTitle(rs.getString(2));
+				board.setMemberId(rs.getString(3));
+				board.setInserted(rs.getTimestamp(4));
+				
+				list.add(board);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public Board get(int id) {
+		String sql = "SELECT id, title, body, memberId, inserted "
+				+ "FROM Board WHERE id=? ";
+		
+		ResultSet rs = null;
+		try(
+			Connection con = DriverManager.getConnection(url, user, password);
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				) {
+			pstmt.setInt(1, id);
+			
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Board board = new Board();
+				board.setId(id);
+				board.setTitle(rs.getString(2));
+				board.setBody(rs.getString(3));
+				board.setMemberId(rs.getString(4));
+				board.setInserted(rs.getTimestamp(5));
+				
+				return board;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+}
